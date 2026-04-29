a94414931a1b6ed7ea96c7a62db038d26f13cd7b
Brice Akama <mekumbasamuel509@gmail.com>
2026-04-29 08:57:10 +0100

    staging

--- app/documents-page/components/DocumentCard.tsx
+++ app/documents-page/components/DocumentCard.tsx
@@ -192,7 +192,7 @@ export default function DocumentCard({
         {/* Analytics */}
         <Button variant="ghost" size="sm"
           onClick={() => router.push(`/documents/${doc._id}`)}
-          className="h-8 w-8 p-0" title="Analytics"
+          className="h-8 w-8 p-0 hidden md:flex" title="Analytics"
         >
           <BarChart3 className="h-4 w-4" />
         </Button>
