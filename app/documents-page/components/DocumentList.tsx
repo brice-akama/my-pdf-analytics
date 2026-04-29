2a72e9b7f3a9a180c1f69d450c9e1051ec710fc3
Brice Akama <mekumbasamuel509@gmail.com>
2026-04-29 08:34:28 +0100

    Hide analytics button on mobile devices

--- app/documents-page/components/DocumentList.tsx
+++ app/documents-page/components/DocumentList.tsx
@@ -305,7 +305,7 @@ function TeamDocRow({ doc, isTemplate, hoveredDocId, setHoveredDocId, onOpenPrev
           </Button>
         )}
 
-        <Button variant="ghost" size="sm" className="h-8 w-8 p-0"
+        <Button variant="ghost" size="sm" className="h-8 w-8 p-0 hidden md:flex"
           onClick={() => router.push(`/documents/${doc._id}`)}>
           <BarChart3 className="h-4 w-4" />
         </Button>
